bf2334e48661221068836b9771b88545245132bb
evenlasting <996955087@qq.com>
2018-09-12 23:55:47 +0800

    arduino 3.0
    测量反应时间。
    反应时间到最终分数。

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,38 +1,53 @@
+#include <LiquidCrystal.h>
+
 #include <Servo.h>
 
 Servo myservo1;
+LiquidCrystal lcd(12,11,5,4,3,2);
 
 int pos=0;
-
+int tpush;
 void setup() {
   // put your setup code here, to run once:
+  lcd.begin(16,2);
   Serial.begin(9600);
   myservo1.attach(9);
   pinMode(LED_BUILTIN,OUTPUT);
-  pinMode(2,INPUT);
+  pinMode(6,INPUT);
   digitalWrite(LED_BUILTIN,LOW);
-    Serial.println(check());
+  //  int tpush=check();
+  tpush=300;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  /*int x=analogRead(A0);
-  if (x>1000) myservo1.write(20);
+  int bscore=analogRead(A1);
+  int score=map(bscore,23,1018,2000,15000);
+  Serial.println(score);
+  lcd.clear();
+  lcd.print("auto s:");
+  lcd.print(score);
+  lcd.setCursor(0,2);
+  lcd.print("your s:");
+  long yours=(long)30*score/tpush;
+  lcd.print(yours);
+  int x=analogRead(A0);
+  if (x>11110) myservo1.write(20);
   delay(30);
   myservo1.write(90);
   delay(100);
-  //Serial.println(x);*/
+  //Serial.println(x);
 
     //Serial.println(digitalRead(2));
 }
 
-int check(){ //my check is 330ms
+int check(){
   digitalWrite(LED_BUILTIN,HIGH);
   delay(1000);
   digitalWrite(LED_BUILTIN,LOW);
   delay(1000);
   digitalWrite(LED_BUILTIN,HIGH);
   int time=millis();
-  while (digitalRead(2));
+  while (digitalRead(6));
   return millis()-time;
 }
