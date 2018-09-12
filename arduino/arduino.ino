c927e8f6d4d2364c7161485fddc1016189cf4e9d
evenlasting <996955087@qq.com>
2018-09-12 10:29:18 +0800

    arduino 2.0
    解决测试反应速度

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,10 +1,38 @@
+#include <Servo.h>
+
+Servo myservo1;
+
+int pos=0;
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  myservo1.attach(9);
+  pinMode(LED_BUILTIN,OUTPUT);
+  pinMode(2,INPUT);
+  digitalWrite(LED_BUILTIN,LOW);
+    Serial.println(check());
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int x=analogRead(A0);
-  Serial.println(x);
+  /*int x=analogRead(A0);
+  if (x>1000) myservo1.write(20);
+  delay(30);
+  myservo1.write(90);
+  delay(100);
+  //Serial.println(x);*/
+
+    //Serial.println(digitalRead(2));
+}
+
+int check(){ //my check is 330ms
+  digitalWrite(LED_BUILTIN,HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(1000);
+  digitalWrite(LED_BUILTIN,HIGH);
+  int time=millis();
+  while (digitalRead(2));
+  return millis()-time;
 }
